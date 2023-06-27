eb28017019bd9a6a1910379696df28b94d0a56f7
Glori <128950766+GlorianaZelaya@users.noreply.github.com>
2023-06-27 03:48:17 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let chooseSymbol = SymbolGenerator();
+  document.getElementById("Symbol").innerHTML = chooseSymbol;
+  document.getElementById("symbolDown").innerHTML = chooseSymbol;
+  document.getElementById("genNumber").innerText = numberGenerator();
+};
+
+let numberGenerator = function() {
+  let number = ["A", "1", "2", "3", "4", "5", "6", "7"];
+  let numeroRandom = [Math.floor(Math.random() * number.length)];
+  return number[numeroRandom];
+};
+let SymbolGenerator = function() {
+  let symbolOne = ["❤️", "♦", "♣", "♠"];
+  let simboloUp = [Math.floor(Math.random() * symbolOne.length)];
+  return symbolOne[simboloUp];
 };
